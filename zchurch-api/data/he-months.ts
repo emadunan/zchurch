bc8272fa38c71bc3b8c70e65848ca1a6317372c9
emadunan <emadunan@gmail.com>
2022-05-10 23:05:18 +0200

    feat: Seed bible books' names into database

---
+++ zchurch-api/data/he-months.ts
@@ -0,0 +1,16 @@
+// Hebrew Months
+const hebrew_months = [
+    {"number": 1, "name_en": "Nisan", "name_ar": "نيسان"},
+    {"number": 2, "name_en": "Iyar", "name_ar": "ايار"},
+    {"number": 3, "name_en": "Sivan", "name_ar": "سيوان"},
+    {"number": 4, "name_en": "Tammuz", "name_ar": "تموز"},
+    {"number": 5, "name_en": "Av", "name_ar": "اب"},
+    {"number": 6, "name_en": "Elul", "name_ar": "ايلول"},
+    {"number": 7, "name_en": "Tishrei", "name_ar": "تشرين"},
+    {"number": 8, "name_en": "Cheshvan", "name_ar": "حشوان"},
+    {"number": 9, "name_en": "Kislev", "name_ar": "كسلو"},
+    {"number": 10, "name_en": "Tevet", "name_ar": "طيبيت"},
+    {"number": 11, "name_en": "Shevat", "name_ar": "شباط"},
+    {"number": 12, "name_en": "AdarI", "name_ar": "اذار الاول"},
+    {"number": 13, "name_en": "AdarII", "name_ar": "اذار الثاني"}
+];
